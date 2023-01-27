98151b27b2f1d76d1316d9a0ad862de519bbb972
RamonRMG <fsk866172@gmail.com>
2023-01-27 11:14:03 +0000

    Applied logic and functionality into JS

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+function getComputerChoice() {
+    let computerStr = ["Rock", "Paper", "Scissors"]
+    let computerIdex = Math.floor(Math.random() * computerStr.length);
+    return computerStr[computerIdex].toUpperCase()
+}
+
+function playerSelection(choice){
+    return choice.toUpperCase()
+}
+
+function randomPlay(playerChoice){
+    let computerChoice = getComputerChoice();
+    if (computerChoice === playerChoice){
+        console.log("WoW! You have a draw!")
+    }else if (computerChoice === "ROCK" && playerChoice === "PAPER"){
+        console.log("You Win! Paper beats Rock")
+    }else if (computerChoice === "ROCK" && playerChoice === "SCISSORS"){
+        console.log("You lose! Rock beats Scissors")
+    }else if (computerChoice === "PAPER" && playerChoice === "ROCK"){
+        console.log("You lose! Paper beats Rock")
+    }else if (computerChoice === "PAPER" && playerChoice === "SCISSORS"){
+        console.log("You Win! Scissors beats Paper")
+    }else if (computerChoice === "SCISSORS" && playerChoice === "ROCK"){
+        console.log("You Win! Rock beats Scissors")
+    }else if (computerChoice === "SCISSORS" && playerChoice === "PAPER"){
+        console.log("You lose! Scissors beats Paper")
+    }
+}
+console.log(randomPlay(playerSelection("paper")));
 
 
-let firstGreeting = "Hello"
-let secGreeting = "JavaScript"
+let myRoot = document.getElementById("p").innerHTML = randomPlay(playerSelection("paper"))
 
-
-let myRoot = document.getElementById("p").innerHTML = firstGreeting + secGreeting
-
-console.log("hello")
+//console.log(myRoot)
